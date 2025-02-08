68065198902ab340be62345a9cb78b94c046cc7d
Alston Liu <alstonliu730@gmail.com>
2025-02-08 09:22:32 -0500

    Created files for setting up raspberry pi.

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # MineSweeper
+
+## Overview
+Welcome to our Capstone Team 5 Repository for our MineSweeper Project. We plan to add more so please be patient.
---
+++ drivers/test.py
@@ -0,0 +1,16 @@
+"""
+File: test.py
+Author: Alston Liu
+Date: 01-21-25
+Description: 
+"""
+import RPI.GPIO as GPIO
+import libcamera as camera
+import adafruit_mlx90640 as libthermal
+import matplotlib as plt
+import numpy
+
+def main():
+
+    return 0;
+main();
